57eed00642cc35cd3ed0073eeecc15a838c9ff07
Andrew Schaper <andrew@schaper.io>
2016-03-01 20:04:31 -0800

    Return data as strings

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -29,13 +29,13 @@ class EventsController < ApplicationController
     event = Event.last
     summary = {
       event_id: event.uuid,
-      sleep_score: event.sleep_score,
+      sleep_score: event.sleep_score.to_s,
       start: event.conditions.first.created_at,
       stop: event.conditions.last.created_at,
-      time_asleep: event.total_time_asleep,
-      light: event.average_of(:light).round,
-      temperature: event.average_of(:temperature).round,
-      noise: event.average_of(:noise).round,
+      time_asleep: event.total_time_asleep.to_s,
+      light: event.average_of(:light).round.to_s,
+      temperature: event.average_of(:temperature).round.to_s,
+      noise: event.average_of(:noise).round.to_s,
       movements: event.movements.map(&:created_at),
       date: event.created_at
     }
